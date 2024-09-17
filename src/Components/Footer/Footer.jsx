04fc2c6133ccadbe466ecc1a7a9134b16a738c2d
shahid <wrteamshahid03@gmail.com>
2024-09-17 18:27:43 +0530

    footer responsive issue fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                 </div>
                 <div className='container cpyRightWrapper d-flex align-items-center justify-content-between'>
                     <span className='cpyRight'>copyright © 2024 WRTeam | Powered by WRTeam</span>
-                    <span>
+                    <span className='linksDiv'>
                         <Link href='https://www.wrteam.in/terms-of-use/' title='Terms of Use' target='_blank'>Terms of Use</Link> | <Link href={'https://www.wrteam.in/privacy-policy/'} title='Privacy Policy' target='_blank'>Privacy Policy</Link> | <Link href={'https://www.wrteam.in/copyright-and-disclaimer/'} title='Copyright & Disclaimer' target='_blank'>Copyright & Disclaimer</Link> | <Link href='https://www.wrteam.in/refund-policy/' title='Refund Policy' target='_blank'>Refund Policy</Link>
                     </span>
                 </div>
